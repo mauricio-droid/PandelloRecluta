a014646a84f2080e7c4c20b633529ba801a05fe6
unknown <carlosmauriciosandovalquintero@gmail.com>
2021-07-15 19:25:24 -0500

    Routing

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter,Switch, Route} from "react-router-dom";
+import Login from "../pages/Login";
 
-function App() {
+function Routes() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default Routes;
